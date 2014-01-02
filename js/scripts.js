9a1b0416b2b117f6ef2b0955078dc67b3deaf753
Tomasz Egiert <tomasz.egiert@ansarada.com>
2014-01-02 16:54:00 +1100

    changes to path drawing

--- js/scripts.js
+++ js/scripts.js
@@ -74,7 +74,8 @@
 		
 			var rect = t.paper.canvas.getBoundingClientRect();
 			//t.trajectory.attr({path:"M"+t.startX+","+t.startY+"l"+(e.pageX-t.startX-rect.left-scrollX)+","+(e.pageY-t.startY-rect.top-scrollY)});
-			t.trajectory.attr({path:"M"+t.startX+","+t.startY+"L"+(e.pageX-rect.left-scrollX)+","+(e.pageY-rect.top-scrollY)});
+			//t.trajectory.attr({path:"M"+t.startX+","+t.startY+"L"+(e.pageX-rect.left-scrollX)+","+(e.pageY-rect.top-scrollY)});
+			t.trajectory.attr({path:"M"+t.startX+","+t.startY+"l"+(e.pageX-rect.left-scrollX-t.startX)+","+(e.pageY-rect.top-scrollY-t.startY)});
 		};
 		
 		this.isActive=true;
@@ -86,8 +87,10 @@
 				// get mouse distance from center of tank
 				
 				var rect = t.paper.canvas.getBoundingClientRect();
+				var distX=e.pageX-rect.left-scrollX-t.startX;
+				var distY=e.pageY-rect.top-scrollY-t.startY;
 				
-				t.shoot(e.pageX-rect.left-scrollX,e.pageY-rect.top-scrollY);
+				t.shoot(distX,distY);
 			}
 		};
 	};
@@ -114,7 +117,7 @@
 			// calculate trajectory
 			
 			var trajectory=[];
-			var a=0,y=this.startY,x=this.startX,acceleration=0;
+			var a=0,y=this.startY,x=this.startX+horiz,acceleration=0;
 			/*
 			while(a<100)
 			{
@@ -133,32 +136,35 @@
 			
 			*/
 			a=1;
-			trajectory=[["M",this.startX,this.startY]];
+			trajectory=[["M"+this.startX,this.startY,"R"]];
 			
-			vert-=this.startY;
+			//vert-=this.startY;
+			
+			var highestPoint=0;
 			
 			while(a<100)
-			{
-				//y+=acceleration;
-
-				//y+=a*9.8;
+			{				
+				y=vert*a+0.5*9.8*Math.pow(a,2)+this.startY;
 				
-				y=vert*a+0.5*9.8*Math.pow(a,2);
-				
-				trajectory.push(["L",x+this.startX,y+this.startY]);
+				trajectory.push([x,y]);
 				
 				x+=horiz;
-				acceleration+=9.8;
 				a++;
+				
+				// later when animating the bullet we need to slow it down as it goes up and speed up as it goes down. To do this we will need to interpolate values between beginning/highest and highest lowest. To do that we need to know the highest point so in here as we calculate gravity we also keep track of highest point so dont have to do the same work later
+				
+				if(y<trajectory[highestPoint][1])	highestPoint=a;
+				//console.log(trajectory[highestPoint][1]);
 			}
+			
 			console.log(trajectory.toString());
-			this.drawTrajectory(trajectory);
+			this.drawTrajectory(trajectory, highestPoint);
 		}
 		
 		return false;
 	};
 	
-	Tank.prototype.drawTrajectory=function(trajectory) {
+	Tank.prototype.drawTrajectory=function(trajectory, highestPoint) {
 		
 		var penPosition={x:this.startX,y:this.startY};
 		var trajectoryPoint;
@@ -168,17 +174,67 @@
 		
 		var arr=Raphael.path2curve(trajectory);
 		var normalized_path = arr.toString();
-		//var normalized_path = "M,200,550,L,200,0,L,217,56.9,L,234,123.6,L,251,200.1,L,268,286.4,L,285,382.5";
+		//var normalized_path = "m,10,10,l,200,0,L,217,56.9,L,234,123.6,L,251,200.1,L,268,286.4,L,285,382.5";
 		
 		
 		//normalized_path="M,"+this.startX+","+this.startY+",L,-400,0,L,-404,-5.1,L,-408,-0.3999999999999986,L,-412,14.100000000000001";
 		
-var path1 = this.paper.path(normalized_path).attr({stroke: "red", "stroke-width":1});
+		var path1 = this.paper.path(normalized_path).attr({stroke: "red", "stroke-width":1});
 
 		trajectoryPoint.attr("fill", "red");
 
-		/*for(var i=0;i<trajectory.length;i++)
+		
+		var lowestPoint=trajectory.length-highestPoint;
+		
+		for(var i=lowestPoint;i>=0;i--)
 		{
+			//speed.push(i/lowestPoint*minSpeed);
+		}
+		
+		// convert quickly
+		
+		
+		// get highest point
+		highestPoint=this.startY;
+		
+		var stepsUp=0;
+		
+		for(stepsUp=0;stepsUp<Raphael.getTotalLength(normalized_path);stepsUp++)
+		{
+			if(highestPoint>path1.getPointAtLength(stepsUp).y)
+				highestPoint=path1.getPointAtLength(stepsUp).y;
+			else break;
+		}
+		
+				
+		// also interpolate between beginning and highest point, and highest point and ending
+		
+		// createarray of interpolated values as many as we need to get to highest point
+		
+		var speed=[];
+		
+		var minSpeed=10000; // 1sec
+		var maxSpeed=5000; // 0.01sec
+		
+		for(var i=0;i<=stepsUp;i++)
+		{
+			speed.push(i/stepsUp*minSpeed);
+		}
+		
+		
+		
+		console.log("ddddd");
+
+		/*
+		//for(var i=0;i<normalized_path.length;i++)
+		for(var i=0;i<100;i++)
+		{
+			var x=Raphael.getPointAtLength(normalized_path,i).x;
+			var y=Raphael.getPointAtLength(normalized_path,i).y;
+			
+			trajectoryPoint=this.paper.circle(x,y,5,5);	
+		
+		
 			//console.log(this.penPosition.x+"    "+penPosition.y);
 				
 			//var trajectoryPoint=this.paper.circle(this.startX+trajectory[i]["x"],this.startY+trajectory[i]["y"],1,1);	
@@ -192,19 +248,31 @@ var path1 = this.paper.path(normalized_path).attr({stroke: "red", "stroke-width"
 			penPosition.x=this.startX+trajectory[i]["x"];
 			penPosition.y=this.startY+trajectory[i]["y"];
 			console.log(trajectory[i]["x"]);
+			
 		}	
+		*/
 		
 		// animate bullet 
 		
+		trajectoryPoint=this.paper.circle(0,0,5,5);
+		
+		//console.log(Math.max.apply(null,[10,20,30,22,33,12,1]));
+				
 		(function() {
-			
+		
 			var step=0;
+			(function() {
 			
 			var animation=setInterval(function animateTo()
 			{
-				if(step<trajectory.length)
+			console.log(speed[step]);
+				var x=Raphael.getPointAtLength(normalized_path,step).x;
+				var y=Raphael.getPointAtLength(normalized_path,step).y;
+				
+				if(step<700)
 				{	
-					animation=trajectoryPoint.animate({cx: trajectory[step]["x"], cy:trajectory[step]["y"]}, 1000, "linear");
+					trajectoryPoint.transform("T"+x+","+y);
+					//animation=trajectoryPoint.animate({cx: trajectory[step]["x"], cy:trajectory[step]["y"]}, 1000, "linear");
 					step++;
 				}
 				else
@@ -212,14 +280,13 @@ var path1 = this.paper.path(normalized_path).attr({stroke: "red", "stroke-width"
 					clearInterval(animation);
 					return;
 				}
-			},1000);
+			},speed[step]);
 			
+				//trajectoryPoint.animate({fill: "blue", transform: "s2.0"}, 1000, "linear");; 
+			})();
+		})(speed);
 			
-			
-			//trajectoryPoint.animate({fill: "blue", transform: "s2.0"}, 1000, "linear");; 
-		})();
-			
-			*/	
+		
 	};
 	
 	// main game framework code
